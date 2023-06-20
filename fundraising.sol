945a9f4d8e3832808bb39c9c7ad417c8c231b77c
Piotr Brudny <pbrudny@gmail.com>
2023-06-20 13:12:45 +0200

    Allow to have multiple stable coins per project

--- fundraising.sol
+++ fundraising.sol
@@ -11,7 +11,7 @@ contract FundraisingPlatform {
     struct Project {
         string name;
         uint256 maxCapacity;
-        address stableCoin; //we need stable coin address? Maybe we need multiple stable coins per project?
+        address[] stableCoins;
         mapping(address => uint256) investorTransfers;
     }
 
@@ -19,12 +19,11 @@ contract FundraisingPlatform {
     mapping(uint256 => Project) public projects;
 
     event ClientAdded(address client);
-    event ProjectCreated(uint256 projectId, string name, uint256 maxCapacity, address stableCoin);
+    event ProjectCreated(uint256 projectId, string name, uint256 maxCapacity, address[] stableCoins);
     event MaxCapacityUpdated(uint256 projectId, uint256 newMaxCapacity);
-    event StableCoinTransferred(uint256 projectId, address indexed investor, uint256 amount);
-    event FundsTransferred(uint256 projectId, address indexed client, uint256 amount);
+    event StableCoinTransferred(uint256 projectId, address indexed investor, address stableCoin, uint256 amount);
+    event FundsTransferred(uint256 projectId, address indexed client, address stableCoin, uint256 amount);
 
-    // TODO: maybe I should use ready modifier
     modifier onlyOwner() {
         require(msg.sender == owner, "Only contract owner can perform this action");
         _;
@@ -42,60 +41,67 @@ contract FundraisingPlatform {
     function addClient(address client) external onlyOwner {
         require(client != address(0), "Invalid client address");
         require(!clients[client], "Client already exists");
-        clients[client] = true; // this way it is cheaper to check clients
+        clients[client] = true;
         emit ClientAdded(client);
     }
 
     function createProject(
         string memory name,
         uint256 maxCapacity,
-        address stableCoin
+        address[] memory stableCoins
     ) external onlyClient {
         require(maxCapacity > 0, "Invalid max capacity");
-        require(stableCoin != address(0), "Invalid stable coin address");
+        require(stableCoins.length > 0, "At least one stable coin required");
 
         projectId++;
-        projects[projectId] = Project(name, maxCapacity, stableCoin);
-        emit ProjectCreated(projectId, name, maxCapacity, stableCoin);
+        projects[projectId] = Project(name, maxCapacity, stableCoins);
+        emit ProjectCreated(projectId, name, maxCapacity, stableCoins);
     }
 
-    // allow client to update max capacity
     function updateMaxCapacity(uint256 projectId, uint256 newMaxCapacity) external onlyClient {
-        //make sure the projectId is correct
-        require(projects[projectId].stableCoin != address(0), "Invalid project ID");
+        require(projects[projectId].stableCoins.length > 0, "Invalid project ID");
         require(newMaxCapacity > 0, "Invalid max capacity");
 
         projects[projectId].maxCapacity = newMaxCapacity;
         emit MaxCapacityUpdated(projectId, newMaxCapacity);
     }
 
-    function transferStableCoins(uint256 projectId, uint256 amount) external {
-        require(projects[projectId].stableCoin != address(0), "Invalid project ID");
+    function transferStableCoins(uint256 projectId, address stableCoin, uint256 amount) external {
+        require(projects[projectId].stableCoins.length > 0, "Invalid project ID");
         require(amount > 0, "Invalid transfer amount");
 
-        IERC20 stableCoinToken = IERC20(projects[projectId].stableCoin);
+        bool validStableCoin = false;
+        for (uint256 i = 0; i < projects[projectId].stableCoins.length; i++) {
+            if (projects[projectId].stableCoins[i] == stableCoin) {
+                validStableCoin = true;
+                break;
+            }
+        }
+        require(validStableCoin, "Invalid stable coin for the project");
 
-        // to do the real transfer on stablecoin
-        // so that fundraising contract address gets funds
+        IERC20 stableCoinToken = IERC20(stableCoin);
         stableCoinToken.transferFrom(msg.sender, address(this), amount);
-
-        //to keep track of investments
         projects[projectId].investorTransfers[msg.sender] += amount;
-        emit StableCoinTransferred(projectId, msg.sender, amount);
+        emit StableCoinTransferred(projectId, msg.sender, stableCoin, amount);
     }
 
-    // what is that function for?
-    function transferFunds(uint256 projectId, uint256 amount) external onlyClient {
-        require(projects[projectId].stableCoin != address(0), "Invalid project ID");
+    function transferFunds(uint256 projectId, address stableCoin, uint256 amount) external onlyClient {
+        require(projects[projectId].stableCoins.length > 0, "Invalid project ID");
         require(amount > 0, "Invalid transfer amount");
         require(projects[projectId].investorTransfers[msg.sender] >= amount, "Insufficient funds");
 
-        IERC20 stableCoinToken = IERC20(projects[projectId].stableCoin);
-        // client transfer coins from smart-contract account to his own
-        stableCoinToken.transfer(msg.sender, amount);
+        bool validStableCoin = false;
+        for (uint256 i = 0; i < projects[projectId].stableCoins.length; i++) {
+            if (projects[projectId].stableCoins[i] == stableCoin) {
+                validStableCoin = true;
+                break;
+            }
+        }
+        require(validStableCoin, "Invalid stable coin for the project");
 
-        //it has to update the state of funds for given client's project
+        IERC20 stableCoinToken = IERC20(stableCoin);
+        stableCoinToken.transfer(msg.sender, amount);
         projects[projectId].investorTransfers[msg.sender] -= amount;
-        emit FundsTransferred(projectId, msg.sender, amount);
+        emit FundsTransferred(projectId, msg.sender, stableCoin, amount);
     }
 }
