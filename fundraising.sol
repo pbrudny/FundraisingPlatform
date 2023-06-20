554e2ae6b6eecabb4eafbd158d76432a7b6bab60
Piotr Brudny <pbrudny@gmail.com>
2023-06-20 13:47:05 +0200

    Import openzeppelin libraries

--- fundraising.sol
+++ fundraising.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import the stable coin token contract interface (assuming ERC-20)
-import "./IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// TODO: Use OpenZeppelin libs
-contract FundraisingPlatform {
-    address public owner;
+contract FundraisingPlatform is Ownable {
+    using SafeERC20 for IERC20;
+
     uint256 public projectId;
 
     struct Project {
@@ -21,47 +22,25 @@ contract FundraisingPlatform {
 
     event ClientAdded(address client);
     event ProjectCreated(uint256 projectId, string name, uint256 maxCapacity);
-    event StableCoinAdded(uint256 projectId, address stableCoin);
     event MaxCapacityUpdated(uint256 projectId, uint256 newMaxCapacity);
     event StableCoinTransferred(uint256 projectId, address indexed investor, address stableCoin, uint256 amount);
     event FundsTransferred(uint256 projectId, address indexed client, address stableCoin, uint256 amount);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only contract owner can perform this action");
-        _;
-    }
+    constructor() {}
 
-    modifier onlyClient() {
-        require(clients[msg.sender], "Only authorized clients can perform this action");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-    }
-
+    // Assumptions that we only add clients and clients could create projects themselves
     function addClient(address client) external onlyOwner {
         require(client != address(0), "Invalid client address");
-        require(!clients[client], "Client already exists");
         clients[client] = true;
         emit ClientAdded(client);
     }
 
-    function createProject(
-        string memory name,
-        uint256 maxCapacity
-    ) external onlyClient {
+    function createProject(string memory name, uint256 maxCapacity) external onlyClient {
         require(maxCapacity > 0, "Invalid max capacity");
 
         projectId++;
         projects[projectId] = Project(name, maxCapacity);
         emit ProjectCreated(projectId, name, maxCapacity);
-    }
-
-    function addStableCoin(uint256 projectId, address stableCoin) external onlyClient {
-        require(stableCoin != address(0), "Invalid stable coin address");
-        projects[projectId].acceptedStableCoins[stableCoin] = true;
-        emit StableCoinAdded(projectId, stableCoin);
     }
 
     function updateMaxCapacity(uint256 projectId, uint256 newMaxCapacity) external onlyClient {
@@ -72,13 +51,20 @@ contract FundraisingPlatform {
         emit MaxCapacityUpdated(projectId, newMaxCapacity);
     }
 
+    function addStableCoin(uint256 projectId, address stableCoin) external onlyClient {
+        require(projects[projectId].maxCapacity > 0, "Invalid project ID");
+        require(stableCoin != address(0), "Invalid stable coin address");
+
+        projects[projectId].acceptedStableCoins[stableCoin] = true;
+    }
+
     function transferStableCoins(uint256 projectId, address stableCoin, uint256 amount) external {
         require(projects[projectId].maxCapacity > 0, "Invalid project ID");
         require(amount > 0, "Invalid transfer amount");
+
         require(projects[projectId].acceptedStableCoins[stableCoin], "Invalid stable coin for the project");
 
-        IERC20 stableCoinToken = IERC20(stableCoin);
-        stableCoinToken.transferFrom(msg.sender, address(this), amount);
+        IERC20(stableCoin).safeTransferFrom(msg.sender, address(this), amount);
         projects[projectId].investorTransfers[msg.sender] += amount;
         emit StableCoinTransferred(projectId, msg.sender, stableCoin, amount);
     }
@@ -87,11 +73,15 @@ contract FundraisingPlatform {
         require(projects[projectId].maxCapacity > 0, "Invalid project ID");
         require(amount > 0, "Invalid transfer amount");
         require(projects[projectId].investorTransfers[msg.sender] >= amount, "Insufficient funds");
+
         require(projects[projectId].acceptedStableCoins[stableCoin], "Invalid stable coin for the project");
 
-        IERC20 stableCoinToken = IERC20(stableCoin);
-        stableCoinToken.transfer(msg.sender, amount);
+        IERC20(stableCoin).safeTransfer(msg.sender, amount);
         projects[projectId].investorTransfers[msg.sender] -= amount;
         emit FundsTransferred(projectId, msg.sender, stableCoin, amount);
+    }
+
+    function onlyClient() internal view {
+        require(clients[msg.sender], "Only authorized clients can perform this action");
     }
 }
